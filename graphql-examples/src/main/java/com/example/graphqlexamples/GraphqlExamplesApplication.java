1bfcdd40e2bedc525e735d715ecae82d62ea3767
Sandipan <sandipanchakraborty@live.in>
2023-10-22 01:32:13 +0530

    Adding graphQL examples.

---
+++ graphql-examples/src/main/java/com/example/graphqlexamples/GraphqlExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.graphqlexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlExamplesApplication.class, args);
+    }
+
+}
---
+++ graphql-examples/src/test/java/com/example/graphqlexamples/GraphqlExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graphqlexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
